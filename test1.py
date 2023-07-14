a90a4a5215a2673c3f5dce7a77f61e44791fab76
Daren Hodgson <darjson@gmail.com>
2023-07-14 13:14:02 +0100

    second commit

---
+++ test1.py
@@ -0,0 +1,13 @@
+num = [345, 67, 87, 67]
+# print(num[2])
+# num[1]=167
+# print(num)
+# print(num[0])
+# print(num[1])
+# print(num[2])
+# print(num[3])
+
+x = 0
+while x < len(num):
+    print(num[x])
+    x += 1
